1bcbd698d5881296ef4ff9ad20fa6df8df470ba7
Coacoabro <99926234+Coacoabro@users.noreply.github.com>
2024-01-17 02:40:24 +0000

    Finalized Tier list

--- src/components/HeroPage/RatesContainer.js
+++ src/components/HeroPage/RatesContainer.js
@@ -2,14 +2,53 @@ import React, { useState, useEffect } from 'react';
 import RateCard from '@/components/HeroPage/RateCard';
 import { useQuery, gql } from '@apollo/client';
 
+function standardDeviation(arr) {
+    const n = arr.length;
+    const mean = arr.reduce((a, b) => a + b) / n;
+    const deviation = Math.sqrt(arr.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
+    return deviation
+  }
+  
+function mean(arr) {
+    return arr.reduce((a, b) => a + b) / arr.length;
+    }
+
+function TierCalc(score, PR) {
+    let t
+    if (PR <= 0.005) {
+        t = '?'
+    }
+    else if(score > 3) {
+        t = 'S+'
+    }
+    else if (score >= 2) {
+        t = 'S'
+    }
+    else if (score >= 1) {
+        t = 'A'
+    }
+    else if (score >= 0) {
+        t = 'B'
+    }
+    else if (score >= -1) {
+        t = 'C'
+    }
+    else if (score >= -2) {
+        t = 'D'
+    }
+    else {
+        t = 'F'
+    }
+    return t
+}
+
 function RatesContainer({ heroId , rank, role }) {
 
     const [heroWinRate, setHeroWinRate] = useState(null);
     const [heroPickRate, setHeroPickRate] = useState(null);
     const [heroMatches, setHeroMatches] = useState(null);
     const [totalMatches, setTotalMatches] = useState(0);
-    const [prevTotalMatches, setPrevTotalMatches] = useState(0);
-
+    const [heroTier, setHeroTier] = useState("?");
     const [infoChange, setInfoChange] = useState(true);   
 
     useEffect(() => {
@@ -40,6 +79,8 @@ function RatesContainer({ heroId , rank, role }) {
 
             let highestMonth = 0;
             let total = 0;
+            let arrayWR = [];
+            let arrayPR = [];
 
             if (infoChange) {
 
@@ -52,8 +93,10 @@ function RatesContainer({ heroId , rank, role }) {
                 data.heroStats.winMonth.forEach((winMonth) => {
                     if (winMonth.month === highestMonth) {
                       total += winMonth.matchCount;
+                      arrayWR.push(winMonth.winCount/winMonth.matchCount);
                     }
                   });
+
                 
                 let finalTotal = total/10;
 
@@ -61,19 +104,45 @@ function RatesContainer({ heroId , rank, role }) {
                     finalTotal *= 5;
                 }
 
-                if (finalTotal !== prevTotalMatches && finalTotal !== 0) {
+                if (finalTotal !== 0) {
 
                     data.heroStats.winMonth.forEach((winMonth) => {
+                        if (winMonth.month === highestMonth) {
+                            arrayPR.push(winMonth.matchCount/finalTotal)
+                        }
+                    });
+
+                
+
+                    data.heroStats.winMonth.forEach((winMonth) => {
+
                         if (winMonth.month === highestMonth && winMonth.heroId === heroId) {
                             setHeroWinRate(((winMonth.winCount / winMonth.matchCount)*100).toFixed(2));
                             setHeroMatches(winMonth.matchCount.toLocaleString());
                             setHeroPickRate(((winMonth.matchCount / finalTotal)*100).toFixed(2));
                             setTotalMatches(finalTotal/10);
+    
+                            const sdWR = standardDeviation(arrayWR)
+                            const sdPR = standardDeviation(arrayPR)
+                            const zScoreWR = ((winMonth.winCount / winMonth.matchCount) - mean(arrayWR)) / sdWR;
+                            const zScorePR = ((winMonth.matchCount / finalTotal) - mean(arrayPR)) / sdPR;
+                            console.log(zScoreWR, zScorePR)
+    
+                            let score;
+                            if (zScoreWR < 0) {
+                                score = zScoreWR - zScorePR;
+                            } else {
+                                score = zScoreWR + zScorePR;
+                            }
+
+                            setHeroTier(TierCalc(score, (winMonth.matchCount / finalTotal)))
+                            
+
                         }
                     });
 
+
                     setInfoChange(false);
-                    setPrevTotalMatches(totalMatches);
 
                 }
                 
@@ -93,7 +162,7 @@ function RatesContainer({ heroId , rank, role }) {
             <div className="flex px-20 py-5 justify-between">
 
                 <div className="w-24 h-24 rounded-md border-4">
-                    <div className="text-center text-2xl text-white py-2">S</div>
+                    <div className="text-center text-2xl text-white py-2">{heroTier}</div>
                     <div className="text-center align-bottom text-white">Tier</div>
                 </div>
 
