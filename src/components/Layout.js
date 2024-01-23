d230146ae5c78314c25dc9e5897ba78658630971
Coacoabro <aaLogan9@gmail.com>
2024-01-23 01:13:26 -0500

    Title added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,12 @@
-
+import Head from 'next/head';
 import TopBar from './TopBar';
 
 const Layout = ({children}) => {
   return (
     <div className="layout">
+      <Head>
+        <title>Dotam.gg</title>
+      </Head>
       <TopBar />
       <main>{children}</main>
       
