ef150fa6abf3af921c3ffd805c9ef58c04f602bd
Coacoabro <aaLogan9@gmail.com>
2024-04-08 04:02:29 -0400

    Sitemap added

--- src/pages/basics.js
+++ src/pages/basics.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata = {
+    title: 'DotaM - Basics',
+    description: 'Basics of Dota 2 for new players',
+  }
+
 function Basics() {
     return(
         <div class="text-white">
