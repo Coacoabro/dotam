4cea456aef344c3a3200c7069ad7b64a6cb88225
Coacoabro <aaLogan9@gmail.com>
2024-04-08 14:14:05 -0400

    Remove 67:

--- python/items.py
+++ python/items.py
@@ -21,6 +21,7 @@ cur = conn.cursor() # Open a cursor to perform database operations
 
 cur.execute("SELECT hero_id from heroes;")
 hero_ids = [row[0] for row in cur.fetchall()]
+hero_ids = hero_ids[67:]
 
 constquery = """
     query {
