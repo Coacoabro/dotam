6b61f8cecee41f11a86575c50c3c789c39ad8dc0
Coacoabro <99926234+Coacoabro@users.noreply.github.com>
2024-04-11 21:25:49 +0000

    Fixed neutrals

--- src/components/HeroPage/Builds/ItemsContainer.js
+++ src/components/HeroPage/Builds/ItemsContainer.js
@@ -11,12 +11,14 @@ import itemConstants from '../../../../dotaconstants/build/items.json'
 
 function ItemBuildsContainer({build, boots, starting, main, neutrals}) {
 
+    const [loaded, setLoaded] = useState(false)
     const [neutralArray, setNeutralArray] = useState([])
     const [early, setEarly] = useState([])
     const [core, setCore] = useState([])
     const [late, setLate] = useState([])
 
     useEffect(() => {
+        setLoaded(false)
         if(neutrals && main) {
             setNeutralArray(neutrals)
             if(main.Early[0]){
@@ -42,37 +44,38 @@ function ItemBuildsContainer({build, boots, starting, main, neutrals}) {
                     .sort((a, b) => a.Time - b.Time)
                 );
             } else {setLate("Not enough data")}
+
+            setLoaded(true)
         }
     },[neutrals, main])
 
-    console.log(neutralArray)
-
-    return(
-        <div className="space-y-8">
-            <div className="flex justify-evenly px-2 space-x-8">
-                <div className="space-y-2">
-                    <StartingItems items={starting} />
-                    <Extra boots={boots}/>
+    if(loaded) {
+        return(
+            <div className="space-y-8">
+                <div className="flex justify-evenly px-2 space-x-8">
+                    <div className="space-y-2">
+                        <StartingItems items={starting} />
+                        <Extra boots={boots}/>
+                    </div>
+                    <div>
+                        <h1 className="text-center text-xl text-white underline">ITEM BUILD</h1>
+                        <ItemOrder early={early} core={core} late={late} />
+                    </div>
                 </div>
-                <div>
-                    <h1 className="text-center text-xl text-white underline">ITEM BUILD</h1>
-                    <ItemOrder early={early} core={core} late={late} />
+                <div className="space-y-1">
+                    <h1 className="text-center text-xl text-white underline">NEUTRAL ITEMS</h1>
+                    <div className="flex justify-evenly text-center">
+                        <NeutralItems tier="1" neutrals={neutralArray["Tier 1"]} />
+                        <NeutralItems tier="2" neutrals={neutralArray["Tier 2"]} />
+                        <NeutralItems tier="3" neutrals={neutralArray["Tier 3"]} /> 
+                        <NeutralItems tier="4" neutrals={neutralArray["Tier 4"]} />
+                        <NeutralItems tier="5" neutrals={neutralArray["Tier 5"]} />
+                    </div>
                 </div>
             </div>
-            <div className="space-y-1">
-                <h1 className="text-center text-xl text-white underline">NEUTRAL ITEMS</h1>
-                <div className="flex justify-evenly text-center">
-                    {neutralArray["Tier 1"][0] ? <NeutralItems tier="1" neutrals={neutralArray["Tier 1"]} /> : null}
-                    {neutralArray["Tier 2"][0] ? <NeutralItems tier="2" neutrals={neutralArray["Tier 2"]} /> : null}
-                    {neutralArray["Tier 3"][0] ? <NeutralItems tier="3" neutrals={neutralArray["Tier 3"]} /> : null}   
-                    {neutralArray["Tier 4"][0] ? <NeutralItems tier="4" neutrals={neutralArray["Tier 4"]} /> : null}
-                    {neutralArray["Tier 5"][0] ? <NeutralItems tier="5" neutrals={neutralArray["Tier 5"]} /> : null}
-                </div>
-            </div>
-            
-        </div>
-        
-    )
+        )
+    }
+    
 }
 
 export default ItemBuildsContainer;
