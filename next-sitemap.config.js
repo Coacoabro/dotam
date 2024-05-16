ad440863246ec625e798e32966ab8bde06bc1f88
Coacoabro <99926234+Coacoabro@users.noreply.github.com>
2024-05-16 20:29:30 +0000

    Forced priority

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,11 +8,20 @@ module.exports = {
     const heroDataPath = path.join(__dirname, 'src/json/dota2heroes.json')
     const heroData = JSON.parse(fs.readFileSync(heroDataPath, 'utf8'))
 
-    return heroData.map(hero => ({
+    const otherPaths = [
+        { loc: '/', lastmod: new Date().toISOString(), changefreq: 'daily', priority: 1.0 },
+        { loc: '/basic', lastmod: new Date().toISOString(), changefreq: 'monthly', priority: 0.4 },
+        { loc: '/heroes', lastmod: new Date().toISOString(), changefreq: 'monthly', priority: 0.6 },
+        { loc: '/tier-list', lastmod: new Date().toISOString(), changefreq: 'yearly', priority: 0.8 },
+      ];
+
+    const heroPaths = heroData.map(hero => ({
       loc: `/hero/${hero.id}`,
       lastmod: new Date().toISOString(),
       changefreq: 'daily',
       priority: 0.7
     }));
+
+    return [...otherPaths, ...heroPaths]
   },
 };
