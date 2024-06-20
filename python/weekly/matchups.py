9b330c90dc677866370d838d45e94ea8f1071311
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2024-06-20 01:10:53 +0000

    Added role specific matchups

--- python/weekly/matchups.py
+++ python/weekly/matchups.py
@@ -19,61 +19,103 @@ headers = {'Authorization': f'Bearer {graphql_token}'}
 conn = psycopg2.connect(database_url)
 cur = conn.cursor() # Open a cursor to perform database operations
 
+cur.execute("TRUNCATE TABLE matchups")
+
 cur.execute("SELECT hero_id from heroes;")
 hero_ids = [row[0] for row in cur.fetchall()]
-hero_ids = hero_ids[41:]
+
+cur.execute("SELECT * from rates")
+rates = cur.fetchall()
+# [0] is hero_id, [6] is role, [7] is rank
 
 
 ranks = ['', 'HERALD_GUARDIAN', 'CRUSADER_ARCHON', 'LEGEND_ANCIENT', 'DIVINE_IMMORTAL']
+roles = ['POSITION_1', 'POSITION_2', 'POSITION_3', 'POSITION_4', 'POSITION_5']
 
-for hero_id in hero_ids:
+def getQuery():
+
+    global ranks
+
+    queries = []
+
     for rank in ranks:
-        query = f"""
-            query{{
-                heroStats {{
-                    heroVsHeroMatchup(
-                        {'heroId: ' + str(hero_id)}
+        rank_query = f"""
+            {rank if rank else 'All'}: heroStats {{
+                    matchUp(
+                        take: 124
                         {'bracketBasicIds: ' + rank if rank else ''}
                     ) {{
-                        advantage {{
-                            vs {{
-                                heroId2
-                                winCount
-                                matchCount
-                            }}
-                            with {{
-                                heroId2
-                                winCount
-                                matchCount
-                            }}
+                        heroId
+                        vs {{
+                            heroId2
+                            winCount
+                            matchCount
+                        }}
+                        with {{
+                            heroId2
+                            winCount
+                            matchCount
                         }}
                     }}
                 }}
-            }}
-        """
-
-        response = requests.post(url, headers=headers, json={'query': query}, timeout=600)
-        data = json.loads(response.text)
-
-        matchupsVs = data['data']['heroStats']['heroVsHeroMatchup']['advantage'][0]['vs']
-        matchupsWith = data['data']['heroStats']['heroVsHeroMatchup']['advantage'][0]['with']
-
-        vsFinal = []
-        withFinal = []
-
-        for matchup in matchupsVs:
-            vsFinal.append({'Hero': matchup['heroId2'], 'WR': round((matchup['winCount']/matchup['matchCount'])*100,2), 'Matches': matchup['matchCount']})
+            """
+        queries.append(rank_query)
+            
+    combined_queries = "\n".join(queries)
         
-        vsFinal.sort(key=lambda x: x['WR'], reverse=True)
+    query = f"""
+        query {{
+            {combined_queries}
+        }}
+    """
+    
+    return query
 
-        for matchup in matchupsWith:
-            withFinal.append({'Hero': matchup['heroId2'], 'WR': round((matchup['winCount']/matchup['matchCount'])*100,2), 'Matches': matchup['matchCount']})
+query = getQuery()
+response = requests.post(url, headers=headers, json={'query': query}, timeout=600)
+data = json.loads(response.text)
+
+for rank in ranks:
+
+    rankName = rank if rank else 'All'
+
+    matchupData = data['data'][rankName]['matchUp']
+
+    for role in roles:
+
+        heroesByRR = []
+
+        for rate in rates:
+            if role == rate[6] and rate[7] in rank and rate[9] != '?':
+                heroesByRR.append(rate[0])
+
+        for matchup in matchupData:
+
+            hero_id = matchup['heroId']            
+
+            if hero_id in heroesByRR:
         
-        withFinal.sort(key=lambda x: x['WR'], reverse=True)
+                matchupsVs = matchup['vs']
+                matchupsWith = matchup['with']
 
-        cur.execute("INSERT INTO matchups (hero_id, rank, herovs, herowith) VALUES (%s, %s, %s, %s);", (hero_id, rank, json.dumps(vsFinal), json.dumps(withFinal)))
+                vsFinal = []
+                withFinal = []
 
-        conn.commit() # Commit the transaction
+                for matchup in matchupsVs:
+                    if matchup['heroId2'] in heroesByRR:
+                        vsFinal.append({'Hero': matchup['heroId2'], 'WR': round((matchup['winCount']/matchup['matchCount'])*100,2), 'Matches': matchup['matchCount']})
+                
+                vsFinal.sort(key=lambda x: x['WR'], reverse=True)
+
+                for matchup in matchupsWith:
+                    if matchup['heroId2'] in heroesByRR:
+                        withFinal.append({'Hero': matchup['heroId2'], 'WR': round((matchup['winCount']/matchup['matchCount'])*100,2), 'Matches': matchup['matchCount']})
+                
+                withFinal.sort(key=lambda x: x['WR'], reverse=True)
+
+                cur.execute("INSERT INTO matchups (hero_id, rank, herovs, herowith) VALUES (%s, %s, %s, %s);", (hero_id, rank, json.dumps(vsFinal), json.dumps(withFinal)))
+
+                conn.commit() # Commit the transaction
 
 conn.close()
 
