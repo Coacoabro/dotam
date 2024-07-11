b24bc5c91f770598b56aa4fb0cac9dfc6f839a66
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2024-07-11 19:10:04 +0000

    Updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,18 +5,17 @@ module.exports = {
   siteUrl: 'https://dotam.gg',
   generateRobotsTxt: true,
   async additionalPaths(config) {
-    const heroDataPath = path.join(__dirname, 'src/json/dota2heroes.json')
+    const heroDataPath = path.join(__dirname, 'json/dota2heroes.json')
     const heroData = JSON.parse(fs.readFileSync(heroDataPath, 'utf8'))
 
     const otherPaths = [
         { loc: '/', lastmod: new Date().toISOString(), changefreq: 'daily', priority: 1.0 },
         { loc: '/basics', lastmod: new Date().toISOString(), changefreq: 'monthly', priority: 0.4 },
-        { loc: '/heroes', lastmod: new Date().toISOString(), changefreq: 'yearly', priority: 0.6 },
         { loc: '/tier-list', lastmod: new Date().toISOString(), changefreq: 'daily', priority: 0.8 },
       ];
 
     const heroPaths = heroData.map(hero => ({
-      loc: `/hero/${hero.id}`,
+      loc: `/hero/${hero.url}`,
       lastmod: new Date().toISOString(),
       changefreq: 'daily',
       priority: 0.7
