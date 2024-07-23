d967a7725626141e92e06740f43d62335de9c390
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2024-07-23 20:34:19 +0000

    Added viewport

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import BottomBar from './BottomBar';
 import TopBar from './TopBar';
 import { Analytics } from "@vercel/analytics/react"
@@ -5,6 +6,9 @@ import { Analytics } from "@vercel/analytics/react"
 export default function Layout({children}) {
   return (
     <div className="layout">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <header className='z-10'><TopBar /></header>
       <main className='pt-24'>{children}</main>
       {/* <footer className='bottom-0 pt-96'><BottomBar /></footer> */}
