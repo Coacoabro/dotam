fa324964641c360e1dc60a85dff74c8ed95b9aeb
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2024-07-26 22:16:15 +0000

    Updated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      gridTemplateColumns: {
-        '24': 'repeat(24,minmax(0, 1fr))',
-        '17': 'repeat(17,minmax(0, 1fr))',
-      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      width: {
-        '14': '3.5rem', // You can adjust the value as needed
-        '16': '4rem',   // You can adjust the value as needed
+      fontSize: {
+        '2xs': '0.65rem',
+      },
+      gridTemplateColumns: {
+        '17': 'repeat(17, minmax(0, 1fr))',
+      },
+      colors: {
+        'slate-950': '#0B0D1C',
+        'slate-900': '#1A1C31',
+        'slate-800': '#22253F',
       },
     },
-    
   },
   plugins: [],
-}
+};
