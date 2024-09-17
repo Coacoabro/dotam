0f9f884d1fe20a0c47d90aa030eac86ef69215e2
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2024-09-17 22:28:09 +0000

    Fixed mobile

--- src/components/HeroPage/Pages/Builds/Items/ItemsContainer.js
+++ src/components/HeroPage/Pages/Builds/Items/ItemsContainer.js
@@ -12,7 +12,6 @@ import Link from 'next/link'
 export default function ItemsContainer({build, hero}) {
 
     const [isCarry, setIsCarry] = useState(true)
-    console.log(hero)
 
     useEffect(() => {
         if (build.role == "POSITION_4" || build.role == "POSITION_5") {
@@ -25,9 +24,9 @@ export default function ItemsContainer({build, hero}) {
     
 
     return(
-        <div className='w-full space-y-2'>
-            <div className="h-72 sm:flex lg:justify-between items-start sm:items-center lg:items-start gap-2.5 space-y-2 lg:space-y-0">
-                <div className='px-12 lg:px-0 space-y-4 sm:w-60'>
+        <div className='w-full space-y-4 sm:space-y-2'>
+            <div className="sm:h-72 sm:flex lg:justify-between items-start sm:items-center lg:items-start gap-2.5 space-y-2 lg:space-y-0">
+                <div className='px-12 lg:px-0 space-y-4 sm:w-72'>
                     <Link href={`/hero/${hero.url}/items`} className='flex items-center justify-between mx-auto rounded-lg border border-cyan-200/25 px-5 py-2'>
                         <div>View More Items</div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 sm:w-5 sm:h-5">
@@ -36,14 +35,19 @@ export default function ItemsContainer({build, hero}) {
                     </Link> 
                     <Starting items={build.starting} />
                 </div>
-                <div className='w-1/3 px-10 sm:px-0'><Early items={build.early} /></div>
-                <div className={`w-5/12 sm:hidden lg:block`}>
+                <div className='hidden sm:flex sm:w-1/3 px-10 sm:px-0'>
+                    <Early items={build.early} />
+                </div>
+                <div className={`hidden sm:w-2/5 lg:block`}>
                     <Core items={build.core} matches={build.total_matches} isCarry={isCarry} />
                 </div>
             </div>
-            <div className={`hidden sm:block lg:hidden w-1/2 mx-auto`}>
-                    <Core items={build.core} matches={build.total_matches} isCarry={isCarry} />
-                </div>
+            <div className='sm:w-1/3 lg:hidden px-10 sm:px-0'>
+                <Early items={build.early} />
+            </div>
+            <div className={`block lg:hidden sm:w-1/2 mx-auto`}>
+                <Core items={build.core} matches={build.total_matches} isCarry={isCarry} />
+            </div>
             <div className='w-full sm:w-3/4 sm:mx-auto lg:w-full '>
                 <Late items={build} isCarry={isCarry} />
             </div>
