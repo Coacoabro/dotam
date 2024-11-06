f68ca8130091d079b633f9a8c9a2896feba35699
Coacoabro <aaLogan9@gmail.com>
2024-11-06 01:20:17 -0500

    Small page bug

--- src/components/HeroPage/Pages/Builds/Items/ItemsContainer.js
+++ src/components/HeroPage/Pages/Builds/Items/ItemsContainer.js
@@ -6,19 +6,30 @@ import Early from './Early'
 import Core from './Core'
 import Late from './Late'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 export default function ItemsContainer({build, hero, role}) {
 
+    const router = useRouter()
+    const initPath = router.asPath.split('/').pop()
+    const initOptions = initPath.split("?")[1]
+
+
     const [isCarry, setIsCarry] = useState(role == "POSITION_4" || role == "POSITION_5" ? false : true)
 
     const [lateItems, setLateItems] = useState(build.core[0].late)
+
+    const [currOptions, setCurrOptions] = useState('?' + initOptions)
 
     const handleLate = (data) => {
         setLateItems(data)
     }
 
     useEffect(() => {
+        const initPath = router.asPath.split('/').pop()
+        const initOptions = initPath.split("?")[1]
+        setCurrOptions('?' + initOptions)
         if (role == "POSITION_4" || role == "POSITION_5") {
             setIsCarry(false)
         }
@@ -26,13 +37,13 @@ export default function ItemsContainer({build, hero, role}) {
             setIsCarry(true)
         }
         setLateItems(build.core[0].late)
-    }, [build, role])    
+    }, [build, role, router])    
 
     return(
         <div className='w-full space-y-4 sm:space-y-2'>
             <div className="sm:h-72 sm:flex lg:justify-between items-start sm:items-center lg:items-start gap-2.5 space-y-2 lg:space-y-0">
                 <div className='px-12 lg:px-0 space-y-4 sm:w-72'>
-                    <Link href={`/hero/${hero.url}/items`} className='flex items-center justify-between mx-auto rounded-lg border border-cyan-200/25 px-5 py-2 hover:bg-slate-700'>
+                    <Link href={`/hero/${hero.url}/items${currOptions ? currOptions : null}`} className='flex items-center justify-between mx-auto rounded-lg border border-cyan-200/25 px-5 py-2 hover:bg-slate-700'>
                         <div>View More Items</div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 sm:w-5 sm:h-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
