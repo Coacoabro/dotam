831f7730be64de44548542becc578517793c4ce1
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2024-11-09 00:13:46 +0000

    Some issues fixing

--- src/components/Ads/Google/BottomBarAd.js
+++ src/components/Ads/Google/BottomBarAd.js
@@ -15,7 +15,7 @@ export default function BottomBarAd({slot}) {
     }, [])
 
     return (
-        <div className="hidden sm:inline-block fixed bottom-0 left-0 right-0 flex justify-center bg-slate-900/50 shadow-lg py-1 z-50">
+        <div className="fixed bottom-0 left-0 right-0 flex justify-center bg-slate-900/50 shadow-lg py-1 z-50 mx-auto">
             <Script
                 async
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2521697717608899"
