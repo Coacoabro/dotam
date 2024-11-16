505c1c2adbabe0c20284729e05d855e0533a8554
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2024-11-16 01:51:13 +0000

    Added an x to close ads

--- src/components/Ads/Google/BottomBarAd.js
+++ src/components/Ads/Google/BottomBarAd.js
@@ -4,6 +4,8 @@ import { useRouter } from 'next/router';
 
 export default function BottomBarAd({slot}) {
 
+    const [viewAd, setViewAd] = useState(true)
+
     useEffect(() => {
         if (typeof window !== 'undefined'){
             try {
@@ -15,23 +17,31 @@ export default function BottomBarAd({slot}) {
     }, [])
 
     return (
-        <div className="hidden sm:flex fixed bottom-0 left-0 right-0 justify-center bg-slate-900/50 shadow-lg py-1 z-50 mx-auto">
-            <Script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2521697717608899"
-                crossOrigin="anonymous"
-                strategy="afterInteractive"
-            />
-            <ins
-                className="adsbygoogle"
-                style={{ 
-                    display: "inline-block", 
-                    width: "728px", 
-                    height: "90px"
-                }}
-                data-ad-client="ca-pub-2521697717608899"
-                data-ad-slot={slot}
-            ></ins>
-        </div>
+        <>
+            <div className={`${viewAd ? "" : "sm:hidden"} hidden sm:flex fixed bottom-0 left-0 right-0 justify-center items-center bg-slate-900/50 shadow-lg py-1 z-50 mx-auto`}>
+                <Script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2521697717608899"
+                    crossOrigin="anonymous"
+                    strategy="afterInteractive"
+                />
+                <ins
+                    className="adsbygoogle"
+                    style={{ 
+                        display: "inline-block", 
+                        width: "728px", 
+                        height: "90px"
+                    }}
+                    data-ad-client="ca-pub-2521697717608899"
+                    data-ad-slot={slot}
+                ></ins>
+                <button 
+                    className='w-12 h-12'
+                    onClick={()=>{setViewAd(false)}}
+                >
+                    <img src="/cross.svg" className='text-cyan-200'/>
+                </button>
+            </div>
+        </>
     );
 }
