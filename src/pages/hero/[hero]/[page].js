1b968f4f23862f59269de2613e97a8b2e12f2416
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2024-11-16 00:38:01 +0000

    Updated more ads

--- src/pages/hero/[hero]/[page].js
+++ src/pages/hero/[hero]/[page].js
@@ -76,40 +76,40 @@ export default function HeroPage({ hero, patch, rates }) {
             MetaContent = `Highest rated abilities and talents for ${heroName}.`
             GuideContent = `Abilities and Talents`
             KeywordsContent = ``
-            VerticalSlot = "1448902987"
-            SquareSlot = "6234325805"
-            BottomSlot = "5282810981"
-            MobileSlot = "3499726534"
+            VerticalSlot = "8840790347"
+            SquareSlot = "3768255622"
+            BottomSlot = "4051498092"
+            MobileSlot = "8725240278"
             break
         case 'builds':
             PageComponent = <Builds hero={hero} current_patch={patch} />
             MetaContent = `Highest rated builds for ${heroName}. See where they fit in the meta through DotaM's tiering system.`
             GuideContent = `Builds`
             KeywordsContent = `Dota 2 ${heroName} builds, ${heroName} hero builds, Best Dota 2 ${heroName} builds, Pro player ${heroName} builds, ${heroName} guides, ${heroName} build guides, ${heroName} item builds, ${heroName} skill builds, ${heroName} ability builds, ${heroName} core builds, ${heroName} starting items, ${heroName} early game builds, ${heroName} late game builds, ${heroName} strategies, Optimal ${heroName} builds, Ranked ${heroName} builds, High MMR ${heroName} builds, ${heroName} role-specific builds, ${heroName} support builds, ${heroName} carry builds, ${heroName} mid-lane builds, ${heroName} offlane builds, ${heroName} jungle builds, Patch meta ${heroName} builds, ${heroName} build recommendations, Popular ${heroName} builds, Match-winning ${heroName} builds, Competitive ${heroName} builds, ${heroName} tier list builds, ${heroName} meta builds, Winning ${heroName} strategies, ${heroName} skill leveling, ${heroName} talent builds, ${heroName} item progression, ${heroName} win rate builds, ${heroName} build optimization, ${heroName} build analysis, ${heroName} facet builds, ${heroName} build variations, ${heroName} draft synergy builds, ${heroName} counter builds, ${heroName} matchup builds, Ranked ${heroName} role builds, Team-based ${heroName} builds, Solo queue ${heroName} builds, Patch-specific ${heroName} builds, ${heroName} performance builds, ${heroName} build customization`
-            VerticalSlot = ""
-            SquareSlot = ""
-            BottomSlot = ""
-            MobileSlot = ""
+            VerticalSlot = "1448902987"
+            SquareSlot = "6234325805"
+            BottomSlot = "5282810981"
+            MobileSlot = "3499726534"
             break
         case 'items':
             PageComponent = <Items current_patch={patch} />
             MetaContent = `Highest rated items for ${heroName}. Find the best items from the beginning to the end of the game`
             GuideContent = `Items - Early, Core, Late, Neutrals`
             KeywordsContent = `Dota 2 ${heroName} items, Best ${heroName} item builds, ${heroName} starting items, ${heroName} early game items, ${heroName} core items, ${heroName} late game items, ${heroName} item progression, ${heroName} neutral items, Optimal ${heroName} item builds, Pro player ${heroName} item builds, ${heroName} item guide, Recommended items for ${heroName}, ${heroName} item strategy, ${heroName} item analysis, ${heroName} item timings, ${heroName} item synergy, ${heroName} situational items, ${heroName} item builds for support, ${heroName} item builds for carry, ${heroName} item builds for mid-lane, ${heroName} item builds for offlane, ${heroName} item builds for jungle, Patch meta ${heroName} item builds, ${heroName} item optimization, Popular ${heroName} item choices, Winning item sets for ${heroName}, ${heroName} item tier list, ${heroName} competitive item builds, ${heroName} build paths, ${heroName} draft item strategy, ${heroName} counter item builds, ${heroName} item build recommendations, ${heroName} team-based item builds, Solo queue ${heroName} item builds, ${heroName} item customization, ${heroName} patch-specific item choices, ${heroName} item win rate, ${heroName} item roles, ${heroName} effective items, ${heroName} high MMR item builds, ${heroName} role-specific item builds, ${heroName} build item variations, ${heroName} ranked item builds, ${heroName} core item sets, ${heroName} best early items, ${heroName} neutral item picks, ${heroName} build performance items, ${heroName} item flexibility, ${heroName} winning item builds`
-            VerticalSlot = ""
-            SquareSlot = ""
-            BottomSlot = ""
-            MobileSlot = ""
+            VerticalSlot = "3292950193"
+            SquareSlot = "8942790758"
+            BottomSlot = "3437083138"
+            MobileSlot = "5168031427"
             break
         case 'matchups':
             PageComponent = <Matchups current_patch={patch} />
             MetaContent = `See which heroes counter and synergizes with ${heroName} the best!`
             GuideContent = `Matchups - Counters and Synergies`
             KeywordsContent = ``
-            VerticalSlot = ""
-            SquareSlot = ""
-            BottomSlot = ""
-            MobileSlot = ""
+            VerticalSlot = "9112253086"
+            SquareSlot = "8497838125"
+            BottomSlot = "1365453568"
+            MobileSlot = "6481113091"
             break
         default:
             PageComponent = <div>Page not found</div>
