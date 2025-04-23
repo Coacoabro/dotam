167d923e2b6c6c2baa0b75cb31a6329b9ad0e375
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2025-04-23 06:55:39 +0000

    Updating builds

--- python/daily/builds.py
+++ python/daily/builds.py
@@ -502,7 +502,7 @@ def sendtosql(builds):
     conn = psycopg2.connect(builds_database_url, connect_timeout=600)
     cur = conn.cursor()
 
-    BATCH_SIZE = 10000
+    BATCH_SIZE = 2500
 
     # process = psutil.Process()
     # mem_info = process.memory_info()
@@ -986,7 +986,7 @@ while True:
         else:
             seq_num += 1
 
-        if hourlyDump >= 500:
+        if hourlyDump >= 250:
             print("Sucessfully parsed data!")
             sendtosql(builds)
             break
