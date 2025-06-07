5ed4d8d54c11e030c914841152fd58f5eb4fad1e
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2025-06-07 02:11:11 +0000

    Updated CDN

--- python/daily/cdn.py
+++ python/daily/cdn.py
@@ -42,9 +42,12 @@ client = clickhouse_connect.get_client(
     secure=True
 )
 
-result = client.query('SELECT hero_id FROM heroes')
-rows = result.result_rows
-hero_ids = [row[0] for row in rows]
+# result = client.query('SELECT hero_id FROM heroes')
+# rows = result.result_rows
+# hero_ids = [row[0] for row in rows]
+
+hero_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 119, 120, 121, 123, 126, 128, 129, 131, 135, 136, 137, 138, 145]
+
 
 # Telegram Stuff
 def send_telegram_message(message):
@@ -201,15 +204,19 @@ try:
         core_conditions = []
         if core_rows:
             for row in core_rows:
-                core = "[" + ", ".join(str(x) for x in row['core']) + "]"  # explicit ClickHouse array
-                stmt = f"SELECT {row['hero_id']} AS hero_id, '{row['rank']}' AS rank, '{row['role']}' AS role, {row['facet']} AS facet, {core} AS core"
-                core_conditions.append(stmt)
-            core_values_clause = " UNION ALL\n".join(core_conditions)
+                core_items = "[" + ",".join(str(x) for x in row["core"]) + "]"
+                line = f"({row['hero_id']}, '{row['rank']}', '{row['role']}', {row['facet']}, {core_items})"
+                core_conditions.append(line)
+            core_values_clause = ",\n".join(core_conditions)
         
 
             late_rows = list(client.query(f"""
                 WITH core_keys AS (
-                    {core_values_clause}
+                    SELECT * FROM (
+                        SELECT * FROM VALUES (
+                            {core_values_clause} 
+                        )                   
+                    ) AS core_keys (hero_id, rank, role, facet, core)
                 ),
                 top_late AS (
                     SELECT
