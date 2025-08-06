7e214b432958ce4df043b5ed916c19187fcc20d3
Aaron Logan <99926234+Coacoabro@users.noreply.github.com>
2025-08-06 03:03:06 +0000

    Updated for 7.39d

--- python/constants/initializebuilds.py
+++ python/constants/initializebuilds.py
@@ -137,13 +137,7 @@ def s3_data():
 
     s3 = boto3.client('s3')
 
-    database_url = os.environ.get('DATABASE_URL')
-    conn = psycopg2.connect(database_url)
-    cur = conn.cursor()
-
-    cur.execute("SELECT hero_id from heroes;")
-    hero_ids = [row[0] for row in cur.fetchall()]
-    conn.close()
+    hero_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 119, 120, 121, 123, 126, 128, 129, 131, 135, 136, 137, 138, 145]
 
     Roles = ['POSITION_1', 'POSITION_2', 'POSITION_3', 'POSITION_4', 'POSITION_5']
     Ranks = ['', 'HERALD', 'GUARDIAN', 'CRUSADER', 'ARCHON', 'LEGEND', 'ANCIENT', 'DIVINE', 'IMMORTAL', 'LOW', 'MID', 'HIGH']
@@ -233,9 +227,9 @@ def hero_info():
 
     
 ## All of these are used (INCLUDING HERO INFO!!)
-# get_facets()
-# get_innate()
-# s3_data()
+get_facets()
+get_innate()
+s3_data()
 hero_info()
 
 
